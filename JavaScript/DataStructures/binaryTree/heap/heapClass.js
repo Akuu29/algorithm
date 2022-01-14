4da4a1538fccbd75f3d46e3c940ba0bd7f7feb06
Akuu29 <owner.akiu0529@gmail.com>
2022-01-14 21:55:30 +0900

    Add Add function "parent" && "buildHeap".

--- JavaScript/DataStructures/binaryTree/heap/heapClass.js
+++ JavaScript/DataStructures/binaryTree/heap/heapClass.js
@@ -5,6 +5,10 @@ export class Heap {
     static right(i) {
         return 2 * i + 2;
     }
+    static parent(i) {
+        // i番目の葉ノードの親ノードのインデックス
+        return Math.floor((i - 1) / 2);
+    }
     static maxHeapify(intArr, i) {
         let left = Heap.left(i);
         let right = Heap.right(i);
@@ -19,5 +23,9 @@ export class Heap {
             intArr[biggest] = temp;
             Heap.maxHeapify(intArr, biggest);
         }
+    }
+    static buildMaxHeap(intArr) {
+        let middle = Heap.parent(intArr.length);
+        for(let i = middle; i >= 0; i--) Heap.maxHeapify(intArr, i);
     }
 }
